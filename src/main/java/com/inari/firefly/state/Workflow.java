1fd5c28c7aac5c3e7172cc6aaba2ca08d584bd73
andreas hefti <anhefti@yahoo.de>
2015-12-12 11:28:30 +0100

    copyright

--- src/main/java/com/inari/firefly/state/Workflow.java
+++ src/main/java/com/inari/firefly/state/Workflow.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015, Andreas Hefti, inarisoft@yahoo.de 
+ * Copyright (c) 2015 - 2016, Andreas Hefti, inarisoft@yahoo.de 
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
