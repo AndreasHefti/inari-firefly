e9a733aaf11a922ff5420e7c246b6a7f8538bcea
andreas hefti <anhefti@yahoo.de>
2017-05-24 08:26:06 +0200

    RenderingSystem

--- src/main/java/com/inari/firefly/graphics/rendering/Renderer.java
+++ src/main/java/com/inari/firefly/graphics/rendering/Renderer.java
@@ -1,10 +1,10 @@
 package com.inari.firefly.graphics.rendering;
 
 import com.inari.commons.lang.aspect.Aspects;
-import com.inari.firefly.system.RenderEventListener;
+import com.inari.firefly.system.RenderEvent;
 import com.inari.firefly.system.component.SystemComponent;
 
-public abstract class Renderer extends SystemComponent implements RenderEventListener {
+public abstract class Renderer extends SystemComponent {
 
     public static final SystemComponentKey<Renderer> TYPE_KEY = SystemComponentKey.create( Renderer.class );
     
@@ -18,5 +18,7 @@ public abstract class Renderer extends SystemComponent implements RenderEventLis
     public abstract boolean accept( int entityId, final Aspects aspects );
     
     public abstract boolean dispose( int entityId, final Aspects aspects );
+    
+    public abstract void render( RenderEvent event );
 
 }
--- src/main/java/com/inari/firefly/graphics/rendering/RenderingSystem.java
+++ src/main/java/com/inari/firefly/graphics/rendering/RenderingSystem.java
@@ -1,8 +1,10 @@
 package com.inari.firefly.graphics.rendering;
 
+import java.util.List;
 import java.util.Set;
 
 import com.inari.commons.lang.aspect.Aspects;
+import com.inari.commons.lang.list.DynArray;
 import com.inari.firefly.entity.EntityActivationListener;
 import com.inari.firefly.system.FFContext;
 import com.inari.firefly.system.RenderEvent;
@@ -19,34 +21,80 @@ public final class RenderingSystem
     
     // TODO
     private boolean allowMultipleAcceptance;
+    
+    private DynArray<Renderer> ordering = DynArray.create( Renderer.class, 20 );
+    private DynArray<Renderer> renderer = DynArray.create( Renderer.class, 20 );
 
     RenderingSystem( FFSystemTypeKey<RenderingSystem> systemKey ) {
         super( systemKey );
     }
 
     @Override
-    public final boolean match( Aspects arg0 ) {
+    public final boolean match( final Aspects aspects ) {
         return true;
     }
-
+    
     
 
     @Override
-    public final void entityActivated( int entityId, Aspects aspects ) {
-        // TODO Auto-generated method stub
-        
+    public final void entityActivated( final int entityId, final Aspects aspects ) {
+        for ( int i = 0; i < ordering.capacity(); i++ ) {
+            Renderer renderer = ordering.get( i );
+            if ( renderer == null ) {
+                continue;
+            }
+            
+            if ( !renderer.match( aspects ) ) {
+                continue;
+            }
+            
+            if ( renderer.accept( entityId, aspects ) && !allowMultipleAcceptance ) {
+                return;
+            }
+        }
     }
 
     @Override
-    public final void entityDeactivated( int entityId, Aspects aspects ) {
-        // TODO Auto-generated method stub
-        
+    public final void entityDeactivated( final int entityId, final Aspects aspects ) {
+        for ( int i = 0; i < ordering.capacity(); i++ ) {
+            Renderer renderer = ordering.get( i );
+            if ( renderer == null ) {
+                continue;
+            }
+            
+            if ( !renderer.match( aspects ) ) {
+                continue;
+            }
+            
+            renderer.dispose( entityId, aspects );
+        }
     }
 
     @Override
-    public final void render( RenderEvent event ) {
-        // TODO Auto-generated method stub
+    public final void render( final RenderEvent event ) {
+        for ( int i = 0; i < ordering.capacity(); i++ ) {
+            Renderer renderer = ordering.get( i );
+            if ( renderer == null ) {
+                continue;
+            }
+            
+            renderer.render( event );
+        }
+    }
+    
+    public final void reorder( final List<String> rendererNames ) {
+        DynArray<Renderer> _renderer = DynArray.create( Renderer.class, renderer.size() );
+        ordering.clear();
+        for ( String name : rendererNames ) {
+            for ( Renderer r : _renderer ) {
+                if ( name.equals( r.getName() ) ) {
+                    _renderer.remove( r );
+                }
+                ordering.add( r );
+            }
+        }
         
+        ordering.addAll( _renderer );
     }
 
     @Override
